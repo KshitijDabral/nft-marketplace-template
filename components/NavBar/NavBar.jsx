dbcbe11697db5094c4ee0e7f15921842664a3a79
unknown <kshitijdabral740@gmail.com>
2023-03-13 10:37:37 +0530

    NFT template rafce

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+// import icons
+import {MdNotifications} from "react-icons/md";
+import {BsSearch} from "react-icons/bs";
+import {CgMenuLeft, CgMenuRight} from "react-icons/cg";
+import { Discover, HelpCenter, Notification, profile, sidebar } from './index';
+//internal import
+
+import Style from "./NavBar.module.css";
+
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
