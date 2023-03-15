6e030c58ddf46da1a60103159741f1c52e0526b7
unknown <kshitijdabral740@gmail.com>
2023-03-15 22:49:40 +0530

    NFT template navbar update 2

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -1,7 +1,7 @@
 import Button from "./Button/Button";
 import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
-import HeroSection from "./HeroSection/HeroSection";
+/* import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
 import BigNFTSilder from "./BigNFTSilder/BigNFTSilder";
 import Subscribe from "./Subscribe/Subscribe";
@@ -17,7 +17,7 @@ import Slider from "./Slider/Slider";
 import Brand from "./Brand/Brand";
 import Video from "./Video/Video";
 import Loader from "./Loader/Loader";
-import Error from "./Error/Error";
+import Error from "./Error/Error"; */
 
 export {
   Button,
